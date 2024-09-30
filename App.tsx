13ade399dff86a20a99b0433db07293944e4243b
= <=>
2024-09-30 12:22:23 -0600

    counter

--- App.tsx
+++ App.tsx
@@ -1,36 +1,12 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './src/components/Counter';
 const App = (): React.JSX.Element => {
-  const [count, setCount] = useState(0);
-
-  const suma = () => {
-    setCount(count + 1);
-  };
-
-  const resta = () => {
-    setCount(count - 1);
-    if (count <= 0) {
-      setCount(0);
-    }
-  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Contador: {count}</Text>
-      <View style={styles.containerButton}>
-        <TouchableOpacity style={styles.button} onPress={() => suma()}>
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => resta()}>
-          <Text style={styles.buttonText}>-</Text>
-        </TouchableOpacity>
-
-        {/* <Button title="+" style={styles.button} onPress={() => setCount(count + 1)} />
-      <Button title="-" onPress={() => setCount(count + 1)} /> */}
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Counter />
+    </SafeAreaView>
   );
 };
 
@@ -39,35 +15,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
   },
-  text: {
-    color: '#000',
-    fontSize: 36,
-  },
-  containerButton: {
-    marginVertical: 40,
-    flexDirection: 'row',
-    gap: 10
-  },
-  button: {
-    width: 50,
-    height: 50,
-    borderRadius: 40,
-    backgroundColor: '#4CAF50',
-    justifyContent: 'center',
-    alignItems: 'center',
-    elevation: 5,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-  },
-  buttonText: {
-    fontSize: 26,
-    color: '#fff',
-    fontWeight: 'bold'
-  }
 });
+
+
 
 export default App;
